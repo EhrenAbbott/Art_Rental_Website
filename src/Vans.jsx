9251a8804c18001b943aa82a4cb9b6b73420579b
Ehren Abbott <era12358@gmail.com>
2023-09-22 14:47:05 -0500

    Refactored to conditionally displayartist name and artwor origin if not unknown and changed API art institute of Chicago

--- src/Vans.jsx
+++ src/Vans.jsx
@@ -1,36 +1,41 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Vans(){ 
 
-    const [vans, setVans] = React.useState([])
+export default function Vans(){
+    
+    const [art, setArt] = React.useState([])
 
     React.useEffect(() => { 
-        fetch("https://api.imgflip.com/get_memes")
+        fetch("https://api.artic.edu/api/v1/artworks")
             .then(res => res.json())
-            .then(data => setVans(data.data.memes))  
+            .then(data => setArt(data.data))
     }, []) 
 
-    console.log(vans)
 
-    const mappedVans = vans.map(data => ( 
-        <div ley={data.id} className='van-tile'> 
-            <img src={data.url}/>
-            <div className="van-info"> 
-                <h3>{data.name}</h3>
-                <p>${data.height}<span>/day</span></p>
-            </div>
+    const mappedArt = art.map(data => data.image_id && ( 
+        <div key={data.id} className='van-tile'>
+            <img src={`https://www.artic.edu/iiif/2/${data.image_id}/full/843,/0/default.jpg`} /> 
+            <div className='van-info'>
+                <h3>{data.title}</h3> 
+                { data.place_of_origin && <h5>Origin: {data.place_of_origin}</h5>}
+                { data.artist_title && <p>By: {data.artist_title}</p>}
+                {/* Check to see if below works when there isnt an artist --API changed art so not applicable example exist at this point */}
+                {data.artist_title != true  && <p>Unknown</p>}
+            </div> 
         </div>
-    ))
-    
+    )) 
+  
+
+    console.log(art)
 
 
-
-    return(  
-        <div className='van-list-container'>
-            <div className='van-list'>
-                {mappedVans}
+    return( 
+        <div className='van-list-container'>  
+            <div  className='van-list'>
+                {mappedArt}
             </div>
         </div>
     )
 }
+
