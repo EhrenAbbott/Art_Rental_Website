792cc49fe29c14174d772224caed5a900eb5eafa
Ehren Abbott <era12358@gmail.com>
2023-09-22 15:24:03 -0500

    Added page state and created loadMore function and button

--- src/Vans.jsx
+++ src/Vans.jsx
@@ -1,33 +1,38 @@
 import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 
 export default function Vans(){
     
     const [art, setArt] = React.useState([])
+    const [page, setPage] = React.useState(1)
+
+    function loadMore(){ 
+        setPage(prevPage => prevPage + 1)
+    }
+
+    
 
     React.useEffect(() => { 
-        fetch("https://api.artic.edu/api/v1/artworks?page=2")
+        fetch(`https://api.artic.edu/api/v1/artworks?page=${page}`)
             .then(res => res.json())
             .then(data => setArt(data.data))
-    }, []) 
+    }, [page]) 
 
-    console.log(art)
 
     const mappedArt = art.map(data => data.image_id && ( 
         <div key={data.id} className='van-tile'>
             <img src={`https://www.artic.edu/iiif/2/${data.image_id}/full/843,/0/default.jpg`} /> 
             <div className='van-info'>
                 <h3>{data.title}</h3> 
-                { data.place_of_origin && <h5>Origin: {data.place_of_origin}</h5>}
-                { data.artist_title && <p>By: {data.artist_title}</p>}
-                {data.artist_title === null  && <p>Artist unknown</p>}
+                { data.place_of_origin && <h5>Origin: {data.place_of_origin}</h5> }
+                { data.artist_title && <p>By: {data.artist_title}</p> }
+                { data.artist_title === null  && <p>Artist unknown</p> }
             </div> 
         </div>
     )) 
-  
 
-    console.log(art)
 
 
     return( 
@@ -35,6 +40,7 @@ export default function Vans(){
             <div  className='van-list'>
                 {mappedArt}
             </div>
+            <button onClick={loadMore}>See more</button>
         </div>
     )
 }
