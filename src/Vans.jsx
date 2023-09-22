ef02360a1f205c108cc157b24c64d010b37c8f72
Ehren Abbott <era12358@gmail.com>
2023-09-22 14:53:56 -0500

    Fixed bug causing 'Unknown' to display for all images

--- src/Vans.jsx
+++ src/Vans.jsx
@@ -7,11 +7,12 @@ export default function Vans(){
     const [art, setArt] = React.useState([])
 
     React.useEffect(() => { 
-        fetch("https://api.artic.edu/api/v1/artworks")
+        fetch("https://api.artic.edu/api/v1/artworks?page=2")
             .then(res => res.json())
             .then(data => setArt(data.data))
     }, []) 
 
+    console.log(art)
 
     const mappedArt = art.map(data => data.image_id && ( 
         <div key={data.id} className='van-tile'>
@@ -20,8 +21,7 @@ export default function Vans(){
                 <h3>{data.title}</h3> 
                 { data.place_of_origin && <h5>Origin: {data.place_of_origin}</h5>}
                 { data.artist_title && <p>By: {data.artist_title}</p>}
-                {/* Check to see if below works when there isnt an artist --API changed art so not applicable example exist at this point */}
-                {data.artist_title != true  && <p>Unknown</p>}
+                {data.artist_title === null  && <p>Artist unknown</p>}
             </div> 
         </div>
     )) 
