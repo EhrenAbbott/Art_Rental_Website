028d347127566c4746200bc72dabd3e310f457fa
Ehren Abbott <era12358@gmail.com>
2023-12-05 12:47:20 -0600

    Added and implemented Form in Login.jsx

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData, Form } from "react-router-dom"
 
 
 export function loader({ request }) { 
@@ -11,41 +11,23 @@ export default function Login() {
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
     const message = useLoaderData()
 
-
-    function handleSubmit(e) {
-        e.preventDefault()
-        console.log(loginFormData)
-    }
-
-    function handleChange(e) {
-        const { name, value } = e.target
-        setLoginFormData(prev => ({
-            ...prev,
-            [name]: value
-        }))
-    }
-
     return (
         <div className="login-container">
             <h1>Sign in to your account</h1>
             {message && <h2 className="red">{message}</h2>}
-            <form onSubmit={handleSubmit} className="login-form">
+            <Form  className="login-form">
                 <input
                     name="email"
-                    onChange={handleChange}
                     type="email"
                     placeholder="Email address"
-                    value={loginFormData.email}
                 />
                 <input
                     name="password"
-                    onChange={handleChange}
                     type="password"
                     placeholder="Password"
-                    value={loginFormData.password}
                 />
                 <button>Log in</button>
-            </form>
+            </Form>
         </div>
     )
 
